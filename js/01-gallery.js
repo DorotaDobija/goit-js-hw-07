4a9457d354f6d995d77082ec437803973e852e6c
DorotaDobija <dorota.dobija93@gmail.com>
2023-03-30 14:38:50 +0200

    task 01 done and start task 02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+galleryItems.forEach((picture) =>
+  galleryList.insertAdjacentHTML(
+    "beforeend",
+    `<li style="display: block" <div class="gallery__item">
+    <a class="gallery__link" href="${picture.original}">
+      <img
+        class="gallery__image"
+        src="${picture.preview}"
+        data-source="${picture.original}"
+        alt="${picture.description}"
+      />
+    </a>
+  </div></li>`
+  )
+);
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  basicLightbox
+    .create(
+      `
+<img src="${event.target.dataset.source}">
+`
+    )
+    .show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+galleryItems.forEach((picture) =>
+  galleryList.insertAdjacentHTML(
+    "beforeend",
+    `<li style="display: block" <a class="gallery__item" href="${picture.original}">
+<img class="gallery__image" src="${picture.preview}" alt="${picture.description}" />
+</a></li>`
+  )
+);
