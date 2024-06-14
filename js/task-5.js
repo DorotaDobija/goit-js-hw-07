0aef2c24d0ef98751324d28e91289a700114964d
Dorota Dobija <117933735+DorotaDobija@users.noreply.github.com>
2024-06-14 17:21:05 +0200

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorText = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body")
+
+const changeBgColor = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorText.innerHTML = getRandomHexColor();
+}
+
+changeColorBtn.addEventListener("click", changeBgColor);
