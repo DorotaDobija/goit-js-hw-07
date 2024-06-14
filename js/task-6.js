e1a5367cacc758ed5314f40c9fdfeecd93601191
Dorota Dobija <117933735+DorotaDobija@users.noreply.github.com>
2024-06-14 22:08:32 +0200

    task-6 done

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,39 @@
+const inputEl = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const divEl = document.querySelector("#boxes");
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const createBoxes = (amount) => {
+  amount = inputEl.value;
+
+  if (amount > 0 && amount <= 100) {
+     for (let i = amount; i <= amount && i > 0; i--) { 
+   
+    const box = `<div style = "width: ${20 + (i * 10)}px; height: ${20 + (i * 10)}px; background-color: ${getRandomHexColor()}; margin: 10px"></div>`;
+
+    divEl.insertAdjacentHTML("afterbegin", box);
+    };
+  }
+  inputClear();
+}
+
+const destroyBoxes = () => {
+  divEl.innerHTML = "";
+}
+
+const inputClear = () => {
+  inputEl.value = "";
+}
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
